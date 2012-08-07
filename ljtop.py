88f528d2c8a601bb364e3e6531ddbbd3dfa85b9f
Boris Tatarintsev <ttyv00@gmail.com>
2012-08-07 20:08:38 +0400

    + ability to login using md5 hash of user password

--- ljtop.py
+++ ljtop.py
@@ -3,11 +3,19 @@
 import xmlrpc.client
 import hashlib
 
+from optparse import OptionParser
+
 proxy = xmlrpc.client.ServerProxy("http://livejournal.com/interface/xmlrpc")
 
-def call_method(username, password, method_name, **user_params):
-	challenge = proxy.LJ.XMLRPC.getchallenge()['challenge']
-	hexpass = hashlib.md5(password.encode('utf-8')).hexdigest()	
+def call_method(username, password, method_name, **user_params):	
+
+	if password.startswith('md5:'):
+		# use md5 hash of password
+		hexpass = password[4:]		
+	else:
+		hexpass = hashlib.md5(password.encode('utf-8')).hexdigest()
+
+	challenge = proxy.LJ.XMLRPC.getchallenge()['challenge']	
 	response = hashlib.md5((challenge + hexpass).encode('utf-8')).hexdigest()
 	params =	{
 					'auth_method': 'challenge',
@@ -25,7 +33,7 @@ def call_method(username, password, method_name, **user_params):
 	return method(params)
 
 try:
-	print (call_method('risboo6909', 'xxx', 'getfriends'))
+	print (call_method('risboo6909', 'md5:15faeadc6c85cc0e82518b09e7e8a92d', 'getfriends'))
 except Exception as e:
 	print (e)
 
